3d99cb8af18ac7accef9f77c54acd8579796bd2f
bert_cai <bert_cai@163.com>
2018-12-05 11:31:06 +0800

    实现简单的留言写入数据库与获取留言列表功能

---
+++ express-example/shoutbox/routes/entries.js
@@ -0,0 +1,32 @@
+const Entry = require('../models/entry')
+
+exports.form = (req, res) => {
+    res.render('post', {
+        title: 'Post'
+    })
+}
+
+exports.submit = (req, res, next) => {
+    const data = req.body.entry
+    const user = res.local.user
+    const username = user ? user.name : null
+    const entry = new Entry({
+        username: username,
+        title: data.title,
+        body: data.body
+    })
+    entry.save((err) => {
+        if (err) return next(err)
+        res.redirect('/')
+    })
+}
+
+exports.list = (req,res,next)=>{
+    Entry.getRange(0,-1,(err,entries)=>{
+        if(err) return next(err)
+        res.render('entries', {
+            title:'Entries',
+            entries:entries
+        })
+    })
+}
